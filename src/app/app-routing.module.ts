6a8b52bc27b801da6e2474be62abd06ff5921728
kyleair <kyleair8@gmail.com>
2021-08-25 15:06:47 +0000

    added insights and assets routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssetsComponent } from './assets/assets.component';
+import { InsightsComponent } from './insights/insights.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'assets', component:AssetsComponent},
+  {path:'insights', component:InsightsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
